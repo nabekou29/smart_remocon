755cc371557d71b07f484da4c9fab205f2b8857e
nabekou <nabekou0729@gmail.com>
2019-08-16 02:47:12 +0900

    apikey修正

--- smart_remocon/firebaseConfig.ts
+++ smart_remocon/firebaseConfig.ts
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: process.env.fireBaseApiKey,
+  apiKey: 'AIzaSyCNHcIPxmACC4GOje7tZZW3M-aUUSyd6sk',
   authDomain: 'raspi-home-1823.firebaseapp.com',
   databaseURL: 'https://raspi-home-1823.firebaseio.com',
   projectId: 'raspi-home-1823',
   storageBucket: 'raspi-home-1823.appspot.com',
-  messagingSenderId: process.env.fireBaseMessagingSenderId,
+  messagingSenderId: '398066115568',
   appId: '1:398066115568:web:46c1716c0d169916',
 };
 
--- smart_remocon/next.config.js
+++ smart_remocon/next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  env: {
-    fireBaseApiKey: 'AIzaSyCNHcIPxmACC4GOje7tZZW3M-aUUSyd6sk',
-    fireBaseMessagingSenderId: '398066115568',
-  },
   exportPathMap: async function(defaultPathMap) {
     return {
       '/': { page: '/' },
