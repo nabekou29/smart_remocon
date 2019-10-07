066067cac16f340cad6d3dc99cc56ce2428d4b38
nabekou <nabekou0729@gmail.com>
2019-10-07 17:49:30 +0900

    リモコン追加機能の作成

--- smart_remocon/src/middleware/saga/top.ts
+++ smart_remocon/src/middleware/saga/top.ts
@@ -1,18 +1,35 @@
 import * as api from '../../api';
 
-import { TopActionTypes, initialize } from '../../actions/top';
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { TopActionTypes, initialize, register } from '../../actions/top';
+import { all, call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
+import { Remocon } from '../../interfaces/entities';
 
 /** 初期化 */
 function* runInitialization() {
   try {
-    const remocons = yield call(api.findAllRemocon);
+    const remocons: Remocon[] = yield call(api.findAllRemocon);
     yield put(initialize.succeed({ remocons }));
   } catch (error) {
     yield put(initialize.fail(error));
   }
 }
 
+/** 登録 */
+function* runRegistration(action: ReturnType<typeof register.start>) {
+  try {
+    const remocon: Remocon = yield call(() =>
+      api.createRemocon(action.payload.name)
+    );
+    yield put(register.succeed(action.payload, { remocon }));
+  } catch (error) {
+    yield put(register.fail(action.payload, error));
+  }
+}
+
 export default function* topSaga() {
-  yield all([takeLatest(TopActionTypes.INITIALIZE_START, runInitialization)]);
+  yield all([
+    takeLatest(TopActionTypes.INITIALIZE_START, runInitialization),
+    takeEvery(TopActionTypes.REGISTER_START, runRegistration),
+  ]);
 }
