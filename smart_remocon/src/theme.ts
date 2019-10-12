a302c0ba627ea768bb8efd5fa773c87526f8a38d
nabekou <nabekou0729@gmail.com>
2019-10-13 02:49:13 +0900

    テーマの修正

--- smart_remocon/src/theme.ts
+++ smart_remocon/src/theme.ts
@@ -1,21 +1,23 @@
-import { blue, red } from '@material-ui/core/colors';
-
 import { createMuiTheme } from '@material-ui/core/styles';
 
-// Create a theme instance.
 const theme = createMuiTheme({
   palette: {
+    type: 'dark',
     primary: {
-      main: blue.A400,
+      light: '#005f56',
+      main: '#00897b',
+      dark: '#33a095',
+      contrastText: '#fff',
     },
     secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
+      light: '#3a4cb1',
+      main: '#536dfe',
+      dark: '#758afe',
+      contrastText: '#8f8f8f',
     },
     background: {
-      default: '#f8f8f8',
+      default: '#616161',
+      paper: '#424242',
     },
   },
 });
